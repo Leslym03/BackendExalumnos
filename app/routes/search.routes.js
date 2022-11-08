518de2a89bedb0d845d71372cd6cec47b89afee4
Leslym03 <leslymita03@gmail.com>
2022-11-08 12:45:50 -0500

    search user

--- app/routes/search.routes.js
+++ app/routes/search.routes.js
@@ -1,9 +1,60 @@
-//nombre, año egreso, especialidad, localidad
-
 const route = require('express').Router()
 
 route.get('/test', (req, res) => {
     res.json({message : 'search'})
+})
+
+//Display location
+route.get('/qlocation/(:id)', (req, res) => {
+    req.getConnection((err, conn) =>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT users.CUI, users.mail, users.graduation_year, users.type_user FROM users INNER JOIN profile ON users.CUI = profile.CUI WHERE profile.location LIKE ?',[req.params.id], (err, rows) =>{
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
+})
+
+
+//Display type user
+route.get('/qtypeuser/(:id)', (req, res) => {
+    req.getConnection((err, conn) =>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT CUI, mail, graduation_year FROM users WHERE type_user =  ?',[req.params.id], (err, rows) =>{
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
+})
+
+//Display specialty
+route.get('/qspecialty/(:id)', (req, res) => {
+    req.getConnection((err, conn) =>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT users.CUI, users.mail, users.graduation_year, users.type_user FROM users INNER JOIN profile ON users.CUI = profile.CUI WHERE profile.specialty = ?',[req.params.id], (err, rows) =>{
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
+})
+
+//Display graduation year
+route.get('/qgraduationyear/(:id)', (req, res) => {
+    req.getConnection((err, conn) =>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT CUI, mail, graduation_year, type_user FROM users WHERE graduation_year = ?',[req.params.id], (err, rows) =>{
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
 })
 
 //Display statistics
