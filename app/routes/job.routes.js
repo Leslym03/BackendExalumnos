1d4b6e1c89bccfc6e061a8eae769f16d69489da9
Leslym03 <leslymita03@gmail.com>
2022-11-08 11:51:40 -0500

    display, insert job offer

--- app/routes/job.routes.js
+++ app/routes/job.routes.js
@@ -4,6 +4,30 @@ route.get('/test', (req, res) => {
     res.json({message : 'job'})
 })
 
+//Display job
+route.get('/display', (req, res) => {
+    req.getConnection((err, conn) =>{
+        if(err) return res.send(err)
 
+        conn.query('SELECT * FROM job_offer', (err, rows) =>{
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
+})
+
+//Insert job
+route.post('/insert', (req, res) => {
+    req.getConnection((err, conn) =>{
+        if(err) return res.send(err)
+
+        conn.query('INSERT INTO job_offer set ?', [req.body], (err, rows) =>{
+            if(err) return res.send(err)
+
+            res.send('Insert job success!')
+        })
+    })
+})
 
 module.exports = route
